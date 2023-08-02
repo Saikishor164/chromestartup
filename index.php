49fb08824a8cb0a080f7ff419f83cd7cb345ef9a
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-08-02 15:18:35 +0530

    updated index.php

--- index.php
+++ index.php
@@ -6,7 +6,67 @@
     <title>saikishor Rasala</title>
 </head>
 <body>
-
     
+
 </body>
 </html>
+ 
+<?php
+// Pie Chart 
+// categories=['category 1', 'Category 2', 'category 3', 'Category 4']
+// values = [25, 15, 25, 35]
+//colors = ["blue", "red", "green", "purple"]
+// plt.pie(values, labels=categories, colors=colors,explode=[0,0,0,0.1])
+//plt.title("Pie Graph / Chart Demonstration")
+//plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// categories=['category 1', 'category  2','']
+// vghfghjgfhjfgh
+// gfjgfhj
+//kljfbkdjf
+// dkljfhg
+// kdjfg
+// fdlkldfkjdss
+// dhffdgh
+// dfghdfh
+// gdfgh
+// fdgh
+// fdghfdgh
+// fgh
+// fdghfdghdfgh
+// dfghdfhdfgh
+// fdgh
+// fdghfdghghfgh
+// dfgh
+// dfghdfhdfghdfgh
+
+// fdgh
+// dfghdfhdfghdfghdfgh
+// fdhg
+// dfghdfhfrdgh
+// fdghfdghfdgh
+// dfhg
+// fdghfdghdfghdfghdfgh
+// fdghfdghfdghfdhg
+// dfghdfhfrdghdfgh
+// fdghfdghfdghfgdhdfgh
+// hgfdsdfgh
+// fdghfdfsdghydrfgth
+// fdghfsdh
+
+
+?>
